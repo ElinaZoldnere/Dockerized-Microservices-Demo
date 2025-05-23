f8ead77628e9c0b6096c8a5e475e53125f5c18e0
ElinaZoldnere <elina.zoldnere@gmail.com>
2025-05-23 11:24:02 +0300

    test: add logger for TestTimeConfig

--- insurance-calculator-app/src/test/java/lv/javaguru/travel/insurance/config/TestTimeConfig.java
+++ insurance-calculator-app/src/test/java/lv/javaguru/travel/insurance/config/TestTimeConfig.java
@@ -5,12 +5,16 @@ import java.time.Duration;
 import java.time.LocalDate;
 import java.time.ZoneId;
 import java.time.temporal.ChronoUnit;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
 
 @TestConfiguration
 public class TestTimeConfig {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(TestTimeConfig.class);
 
     @Bean
     @Primary
@@ -23,7 +27,10 @@ public class TestTimeConfig {
         Duration offset = Duration.ofDays(daysOffset);
 
         // Provides a ticking Clock shifted to the same day and month in the year 2024.
-        return Clock.offset(Clock.system(zone), offset);
+        Clock shiftedClock = Clock.offset(Clock.system(zone), offset);
+        LOGGER.info("TestTimeConfig initialized with simulated date: {} (Zone: {})",
+                LocalDate.now(shiftedClock), zone);
+        return shiftedClock;
     }
 
 }
